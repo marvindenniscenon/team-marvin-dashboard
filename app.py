7f4641a0b265b58b443364a585daab0697d65924
marvindenniscenon <7k8vbt7rcm@privaterelay.appleid.com>
2026-05-26 13:08:48 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ import pandas as pd
 
 st.title("TEAM MARVIN INTAKE DASHBOARD")
 
-df = pd.read_excel("TEAM MARVIN INTAKE FILE New app.xlsx")
+df = pd.read_excel("TEAM MARVIN INTAKE FILE New app.xlsx",header=4)
 
 st.dataframe(df)
